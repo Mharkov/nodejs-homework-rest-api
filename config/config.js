dc88f91150f3f46922269976b9ed0d7c56942ea3
Maksym Chubarov <maximuskh@gmail.com>
2021-06-15 23:06:07 +0300

    finish

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  email: {
+    sendgrid: 'maximuskh@gmail.com',
+    nodemailer: 'maximuskh@gmail.com',
+  },
+  link: {
+    localhost: 'http://localhost:3000',
+    domenName: 'Link for production',
+  },
+};
